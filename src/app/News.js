27717cdfef8a09a939cbd1d13dffb33bc9978dec
dem0-0 <emirkan.derkoken1@gmail.com>
2023-08-23 18:38:58 +0300

    News reading page almost done (summary highlighting left)

--- src/app/News.js
+++ src/app/News.js
@@ -17,10 +17,48 @@ function News() {
         ).then((res) => res.json()).then((data) => setNewsData(data))
     }, [title])
 
+    const parseURL = () => (newsData.link === undefined) ? "" : new URL(newsData.link).hostname
+
+    const parseSentiment = () => {
+        if (newsData.sentiment === undefined) {
+            return ""
+        }
+
+        const dominantSentiment = Object.keys(newsData.sentiment).reduce((a, b) => newsData.sentiment[a] > newsData.sentiment[b] ? a : b);
+
+        return (
+            <div>
+                <span style={{color: "red", fontWeight: "bold"}}>{newsData.sentiment[dominantSentiment]}%</span> {dominantSentiment.charAt(0).toUpperCase() + dominantSentiment.slice(1)}
+            </div>
+        )
+    }
+
     return (
-        <div>
-            <h1> {title} </h1>
-            <p> {JSON.stringify(newsData)} </p>
+        <div className="outer-div">
+            <div className="title-and-hr-container">
+                <h1 className="title"> {title} </h1>
+                <div className="red-hr" /> 
+            </div>
+
+            <div className="link-and-sentiment">
+                <div className="link">
+                    <a href={newsData.link}> {title} | ({parseURL()}) </a>
+                </div>
+                <div className="sentiment">
+                    {parseSentiment()}
+                </div>
+            </div>
+
+            <div className='new-body'>
+                {newsData.body}
+            </div>
+            <div className='new-body' style={{padding: "3% 7% 0% 7%"}}>
+                {newsData.body}
+            </div>
+            <div className='new-body' style={{padding: "3% 7% 0% 7%"}}>
+                {newsData.body}
+            </div>
+            
         </div>
     );
 }
