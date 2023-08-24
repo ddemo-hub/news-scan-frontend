dc4ab2d92b8a18679ef2f38577294bf49554ef78
Emin Buğra Türkeli <T097714@kfs.local>
2023-08-24 12:14:02 +0300

    hovers added

--- src/components/NewsGrid.js
+++ src/components/NewsGrid.js
@@ -7,9 +7,9 @@ function NewsGrid() {
         <div class='NewsGrids'>
           <div class='left-part'>
             <div class='news-entry'>
-              <p class= 'news-title'>
+              <h1 class= 'news-title'>
                 Icardi Trabzonspor'a 2 tane tıklattı
-              </p>
+              </h1>
               <p class='news-teaser'>
                 Ünlü Arjantinli golcü Mauro Emmanuel Icardi Trabzonspor karşısında iki gol atarak, etkileyici bir performans sergiledi
               </p>
@@ -98,18 +98,18 @@ function NewsGrid() {
               <p class='stats-title'>TODAY'S TRENDS</p>
               <div class='stats-contents'>
                 <p class='stats-subjects'>Trending topics:</p>
-                <p class='stats-article'>&#x2022;Transfer Çalımı</p>
-                <p class='stats-article'>&#x2022;Futbol</p>
+                <p class='stats-article'>&#x2022; Transfer Çalımı</p>
+                <p class='stats-article'>&#x2022; Futbol</p>
               </div>  
               <div class='stats-contents'>
                 <p class='stats-subjects'>Popular People:</p>
-                <p class='stats-article'>&#x2022;Icardi</p>
-                <p class='stats-article'>&#x2022;Wanda Nara</p>
+                <p class='stats-article'>&#x2022; Icardi</p>
+                <p class='stats-article'>&#x2022; Wanda Nara</p>
               </div>  
               <div class='stats-contents'>
-                <p class='stats-subjects'>&#x2022;Popular Organizations:</p>
-                <p class='stats-article'>&#x2022;Trabzonspor</p>
-                <p class='stats-article'>&#x2022;Salatasaray</p>
+                <p class='stats-subjects'>&#x2022; Popular Organizations:</p>
+                <p class='stats-article'>&#x2022; Trabzonspor</p>
+                <p class='stats-article'>&#x2022; Salatasaray</p>
               </div>  
             </div>
 
@@ -118,13 +118,13 @@ function NewsGrid() {
               <div class='stats-contents'>
                 <p class='stats-subjects'>Sentiments:</p>
                 <p class='stats-article'>
-                  <span class='boldpart'>&#x2022;86%</span> of the news are <span class='boldpart'>negative</span>
+                  <span class='boldpart'> &#x2022;86%</span> of the news are <span class='boldpart'>negative</span>
                 </p>
                 <p class='stats-article'>
-                  <span class='boldpart'>&#x2022;10%</span> of the news are <span class='boldpart'>neutral</span>    
+                  <span class='boldpart'> &#x2022;10%</span> of the news are <span class='boldpart'>neutral</span>    
                 </p>
                 <p class='stats-article'>
-                  <span class='boldpart'>&#x2022;4%</span> of the news are <span class='boldpart'>positive</span>  
+                  <span class='boldpart'> &#x2022;4%</span> of the news are <span class='boldpart'>positive</span>  
                 </p>
               </div>  
             </div>
